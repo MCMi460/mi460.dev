63325f36d74a6ea4e1f1abc1dfb49d90c59c5ca3
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-08-13 17:46:40 -0500

    Some much needed updates

--- static/js/script.js
+++ static/js/script.js
@@ -11,11 +11,21 @@ const avatars = [
 ]
 var avatar_index = 0;
 
-function switch_avatar() {
-  avatar_index++;
+function switch_avatar(choice = null) {
+  if (choice != null) {
+    avatar_index = choice;
+  } else {
+    avatar_index++;
+  }
   if (avatar_index >= avatars.length) {
+    avatar_index = 0;
+  } else if (avatar_index < 0) {
     avatar_index = 0;
   }
   document.getElementById('avatar').src = 'static/assets/avatars/' + avatars[avatar_index];
   //return avatars[avatar_index];
+}
+
+function load() {
+  switch_avatar(Math.floor(Math.random() * avatars.length)); // Random choice of avatar at load
 }
